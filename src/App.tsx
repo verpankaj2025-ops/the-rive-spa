6c6f0d1c4eeeb91a6826fc36804502fc9a5e6db7
pankajverma <YOUR_GMAIL@gmail.com>
2026-09-21 02:21:21 +0530

    Fix clean routes for SEO indexing

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { HashRouter, Routes, Route, useLocation } from 'react-router-dom';
+import { BrowserRouter, Routes, Route, useLocation } from 'react-router-dom';
 import { useEffect } from 'react';
 import Layout from './components/Layout';
 import Home from './pages/Home';
@@ -9,31 +9,59 @@ import ServiceDetail from './pages/ServiceDetail';
 
 function ScrollToTop() {
   const { pathname } = useLocation();
-  useEffect(() => { window.scrollTo(0, 0); }, [pathname]);
+
+  useEffect(() => {
+    window.scrollTo(0, 0);
+  }, [pathname]);
+
   return null;
 }
 
 function PageTracker() {
   const location = useLocation();
+
   useEffect(() => {
     window.dataLayer = window.dataLayer || [];
-    window.dataLayer.push({ event: 'page_view', page_title: document.title, page_path: location.pathname });
+
+    window.dataLayer.push({
+      event: 'page_view',
+      page_title: document.title,
+      page_path: location.pathname,
+    });
   }, [location]);
+
   return null;
 }
 
 export default function App() {
   return (
-    <HashRouter>
+    <BrowserRouter>
       <ScrollToTop />
       <PageTracker />
+
       <Routes>
         <Route path="/" element={<Layout><Home /></Layout>} />
-        <Route path="/services" element={<Layout><Services /></Layout>} />
-        <Route path="/services/:slug" element={<Layout><ServiceDetail /></Layout>} />
-        <Route path="/about" element={<Layout><About /></Layout>} />
-        <Route path="/contact" element={<Layout><Contact /></Layout>} />
+
+        <Route
+          path="/services"
+          element={<Layout><Services /></Layout>}
+        />
+
+        <Route
+          path="/services/:slug"
+          element={<Layout><ServiceDetail /></Layout>}
+        />
+
+        <Route
+          path="/about"
+          element={<Layout><About /></Layout>}
+        />
+
+        <Route
+          path="/contact"
+          element={<Layout><Contact /></Layout>}
+        />
       </Routes>
-    </HashRouter>
+    </BrowserRouter>
   );
 }
